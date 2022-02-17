757c4a2a0e73c3005362cbc71fc76619775653ca
Shuvadip ghosh <shuvadipdeveloper@gmail.com>
2022-02-17 22:13:27 +0530

    made the custom.bat file to start the virtualenv if the name is passed

--- custom.bat
+++ custom.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
+set arg1=%1
 color 09
 prompt [%username%@%computername%]-[$p]$$ 
+CALL %arg1%\Scripts\activate
 cls
 echo Microsoft Windows [Version 10.0.19043.1348] 
 echo (c) Microsoft Corporation. All rights reserved.
